4a20628ffe51f18d0c45c569f23f7460fddeade2
Savio Marques <savio.marques@sap.com>
2024-08-04 20:30:46 +0000

    Action

--- srv/service.cds
+++ srv/service.cds
@@ -4,4 +4,6 @@ namespace capgenfilter;
 // Definição do serviço
 service CatalogService {
     function GenerateFilter(Text: String, Entity: String, Field: String) returns String;
+
+    action GenerateFilterAction(Text: String, Entity: String, Field: String) returns String;
 }
--- srv/service.js
+++ srv/service.js
@@ -17,4 +17,21 @@ module.exports = (srv) => {
         return encodeURIComponent(mainFilter);
 
     });
+
+    srv.on('GenerateFilterAction', async (req) => {
+        const { Text , Entity, Field } = req.data;
+
+     
+        // Dividir o texto em palavras
+        const palavras = Text.split(" ");
+
+        // Construir os filtros de pesquisa
+        const filters = palavras.map(palavra => `contains(d/${Field},'${palavra}')`).join(' or ');
+
+        const mainFilter = `${Entity}/any(d: d/Language eq 'PT' and (${filters}))`;
+
+        // Construir a URL
+        return encodeURIComponent(mainFilter);
+
+    });
 };
